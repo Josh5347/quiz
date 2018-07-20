4f0b104d632fe1a274ebdec2f8a61e6ab58b588f
Josh <josh@example.com>
2018-07-20 16:02:07 +0800

    js change

--- js/quiz.js
+++ js/quiz.js
@@ -1,4 +1,24 @@
 $(document).ready(function(){
-    var no = $('table tr td:first').text();
-    console.log(no);
+    var question = [];
+    $('.qContent').each(function(){
+        question.push($(this).text());
+        //console.log($(this).text());
+    });
+
+    var qClass = [];
+    $('.qClass').each(function(){
+        qClass.push($(this).text());
+        //console.log($(this).text());
+    });     
+
+    var qAnswer = [];
+    $('.qAnswer').each(function(){
+        qAnswer.push($(this).text());
+        //console.log($(this).text());
+    });     
+    //qAnswer.forEach(function (val) { 
+    //    console.log(val);
+    //});
+
+
 });
